6acb76e4ca829926d2ea162b1a44ac36a05d0e53
Berdan Koca <berdankoca@gmail.com>
2016-01-26 21:18:27 +0200

    first initial

---
+++ SecondCI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecondCI.Startup))]
+namespace SecondCI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
